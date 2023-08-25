f310c429f69ff5b0bc3e57427efd63188d429143
Maxim Romanyuk <metheoryt@gmail.com>
2023-08-26 03:13:59 +0600

    fix chat_id sqlalchemy type

--- telegrind/models.py
+++ telegrind/models.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import BigInteger
 
 
 class Model(AsyncAttrs, DeclarativeBase):
@@ -11,7 +12,7 @@ class Chat(Model):
     __tablename__ = 'chat'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    chat_id: Mapped[int]
+    chat_id: Mapped[int] = mapped_column('chat_id', BigInteger)
     sheet_url: Mapped[Optional[str]]
 
 
